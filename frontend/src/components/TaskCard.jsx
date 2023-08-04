30b906888ddfacad4ffa236713cb67fab1e26ae6
Dylan033 <yampuezandylan04@gmail.com>
2023-08-04 15:35:52 -0500

    delete component

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,22 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-// function TaskCard({task}) {
-
-//   const navigate = useNavigate()
-
-//   return (
-//     <div
-//     className="bg-zinc-800 p-3 mr-3 hover:bg-zinc-700 hover:cursor-pointer hover:scale-105 transition-all duration-200 rounded-md"
-//     onClick={() => {
-//       navigate(`/tasks/${task.id}`)
-//     }}>
-//       <p className="font-bold uppercase">{task.title}</p>
-//       <p className="text-slate-400">{task.description}</p>
-//     </div>
-//   );
-// }
-
 function TaskCard({ task }) {
   // const { deleteTask, completedTask } = useContext(TaskContext);
   const navigate = useNavigate()
@@ -33,7 +17,7 @@ function TaskCard({ task }) {
       <p className="text-center">{task.description}</p>
       <button
         className="bg-red-500 px-2 py-1 rounded-md mt-4 mx-auto flex"
-        // onClick={() => deleteTask(task.id)}
+        onClick={() => deleteTask(task.id)}
       >
         Borrar Tarea
       </button>
@@ -41,7 +25,7 @@ function TaskCard({ task }) {
         className={`bg-green-500 px-2 py-1 rounded-md mt-4 mx-auto flex  ${
           task.completed ? "bg-black" : ""
         }`}
-        // onClick={() => completedTask(task.id)}
+        onClick={() => completedTask(task.id)}
       >
         Completada
       </button>
